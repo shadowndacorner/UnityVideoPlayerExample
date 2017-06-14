4898b05a2ddf051e061f5f706adf8d734aed754d
Ian Diaz <shadowndacorner@gmail.com>
2017-06-14 15:58:22 -0500

    Initial commit

---
+++ Assets/RenderTextureToImage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderTextureToImage : MonoBehaviour {
+    private RenderTexture m_tex;
+    public RenderTexture Texture
+    {
+        get
+        {
+            return m_tex;
+        }
+        set
+        {
+            m_tex = value;
+            GetComponent<RawImage>().texture = m_tex;
+        }
+    }
+	
+}
---
+++ Assets/VideoPlayer/Example/VideoPlayerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerTest : MonoBehaviour {
+    public string URL;
+    public RenderTexture MovieTexture;
+    public RenderTextureToImage UI_Image;
+
+    void Start () {
+        var player = GetComponent<VideoPlayer>();
+        if (!player)
+            player = gameObject.AddComponent<VideoPlayer>();
+        
+        MovieTexture = new RenderTexture(1280, 720, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+        if (UI_Image)
+        {
+            UI_Image.Texture = MovieTexture;
+        }
+
+
+		player.url = URL;
+        player.renderMode = VideoRenderMode.RenderTexture;
+        player.targetTexture = MovieTexture;
+        player.Play();
+
+        GetComponent<MeshRenderer>().material.mainTexture = MovieTexture;
+	}
+	
+}
